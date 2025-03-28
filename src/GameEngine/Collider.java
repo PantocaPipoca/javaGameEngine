87e59b3cf18c3189c5a9eeb2f41315f1e1f02d8c
Pantas <109051590+PantocaPipoca@users.noreply.github.com>
2025-03-28 18:30:55 +0000

    Finished L

--- src/GameEngine/Collider.java
+++ src/GameEngine/Collider.java
@@ -8,15 +8,19 @@ public class Collider implements ICollider{
     private FiguraGeometrica figuraDoCollider;
     public Collider(FiguraGeometrica f, ITransform t) {
         
+        // Cria uma copia do preset da figura do collider para reuso futuro (Tambem pq e imutavel ne)
         FiguraGeometrica preset = f.clone();
 
+        // Pega o centroide do preset para sabermos o ponto partida
         Ponto presetPos = preset.centroide();
+
+        // Aplica as transformacoes no preset
         preset = preset.scale(t.scale());
-
+        // Move o preset de maneira a que o centroide fique na posicao do centroide do transform
         preset = preset.translate(t.position().x() - presetPos.x(), t.position().y() - presetPos.y());
-
         preset = preset.rotate(t.angle(), t.position());
 
+        // Temos uma copia do nosso preset de acordo com o modelo gui
         figuraDoCollider = preset;
 
     }
